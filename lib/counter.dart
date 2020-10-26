0f54e39c881be73ca6b2de5ddf08398e69de13e2
tonyPC <tontruong@mpire.vn>
2020-10-26 13:23:51 +0700

    init up

---
+++ lib/counter.dart
@@ -0,0 +1,26 @@
+// library faker.example;
+
+import 'package:faker/faker.dart';
+import 'package:flutter/widgets.dart';
+import 'package:logger/logger.dart';
+
+class Counter with ChangeNotifier {
+  var txt = "0";
+  var faker = new Faker();
+  var logger = new Logger();
+
+  void changeRandomTxt() {
+    // txt = faker.sport.name();
+    txt = faker.food.cuisine();
+
+    // logger.v("Verbose log");
+    // logger.d("Debug log");
+    // logger.i("Info log");
+    // logger.w("Warning log");
+    // logger.e("Error log");
+    // logger.wtf("What a terrible failure log");
+    logger.d(txt);
+
+    notifyListeners();
+  }
+}
